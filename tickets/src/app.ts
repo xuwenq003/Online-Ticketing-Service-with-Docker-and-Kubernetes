9ff164571655cdbe5c624827c1634378ef5e33bf
Peter Qiu <peterqiu971112@gmail.com>
2022-07-13 19:49:06 -0400

    ticket service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,8 @@ import cookieSession from 'cookie-session';
 import {errorHandler, NotFoundError, currentUser} from '@xqtickets/common';
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 
 const app = express();
@@ -21,6 +23,8 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 
 app.all('*', async (req, res) => {
