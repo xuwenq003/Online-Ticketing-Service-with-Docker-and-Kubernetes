2152e521f3622c8d49c6558acf6ede31ed2b7cee
Peter Qiu <peterqiu971112@gmail.com>
2022-07-14 22:02:30 -0400

    connect to nats

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@xqtickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
 
 const router = express.Router();
 
@@ -19,6 +20,12 @@ async (req: Request, res: Response) => {
     userId: req.currentUser!.id
   });
   await ticket.save();
+  // new TicketCreatedPublisher(client).publish({
+  //   id: ticket.id,
+  //   title: ticket.title,
+  //   price: ticket.price,
+  //   userId: ticket.userId
+  // });
 
   res.status(201).send(ticket);
 });
