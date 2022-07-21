6d89aa85f461942912f84e8a04903cfc54923d99
Peter Qiu <peterqiu971112@gmail.com>
2022-07-21 19:18:51 -0400

    order starter code

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,11 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import {errorHandler, NotFoundError, currentUser} from '@xqtickets/common';
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
-
+import { deleteOrderRouter } from './routes/delete';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { indexOrderRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -21,10 +20,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 
 app.all('*', async (req, res) => {
