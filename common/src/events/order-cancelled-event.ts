daa47796462346f96dd6257b8bbdb43e2ce1f6ba
Peter Qiu <peterqiu971112@gmail.com>
2022-07-23 21:48:06 -0400

    order publisher

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -2,5 +2,11 @@ import { Subjects } from "./subjects";
 
 
 export interface OrderCancelledEvent {
-  
+  subject: Subjects.OrderCancelled;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
 }
--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,12 +1,14 @@
 import express, {Request, Response} from 'express';
 import { NotAuthorizedError, NotFoundError, requireAuth } from '@xqtickets/common';
 import { Order, OrderStatus } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
   const {orderId} = req.params;
-  const order = await Order.findById(orderId);
+  const order = await Order.findById(orderId).populate('ticket');
   if (!order) {
     throw new NotFoundError();
   }
@@ -17,6 +19,13 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
   await order.save();
   
   // publishing an event saying this was cancelled!
+  new OrderCancelledPublisher(natsWrapper.client).publish({
+    id: order.id,
+    ticket: {
+      id: order.ticket.id,
+    },
+  });
+
   res.status(204).send(order);
 });
 
