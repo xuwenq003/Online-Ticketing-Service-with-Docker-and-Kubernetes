19ec38358a894b6720b01289aad289e539e2e77b
Peter Qiu <peterqiu971112@gmail.com>
2022-07-25 15:11:52 -0400

    concurrent working out

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -34,7 +34,8 @@ async (req: Request, res: Response) => {
     id: ticket.id,
     title: ticket.title,
     price: ticket.price,
-    userId: ticket.userId
+    userId: ticket.userId,
+    version: ticket.version,
   });
 
   res.send(ticket);
