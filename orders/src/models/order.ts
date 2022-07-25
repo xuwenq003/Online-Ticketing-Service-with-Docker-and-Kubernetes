d288da4c2adb1da8039ef68a5e640a6a0d839b48
Peter Qiu <peterqiu971112@gmail.com>
2022-07-25 19:11:50 -0400

    move to ticket

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import {updateIfCurrentPlugin} from 'mongoose-update-if-current';
 import {TicketDoc} from './ticket';
 import {OrderStatus} from '@xqtickets/common';
 
@@ -50,6 +51,8 @@ const orderSchema = new mongoose.Schema({
   }
 });
 
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
